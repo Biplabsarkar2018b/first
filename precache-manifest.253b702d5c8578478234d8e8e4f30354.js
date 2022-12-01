1d32b7f02da260a39d8063b822ee45b88369194f
Biplab Sarkar <94435217+Biplabsarkar2018b@users.noreply.github.com>
2022-12-01 15:17:57 +0530

    Updates

---
+++ precache-manifest.253b702d5c8578478234d8e8e4f30354.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cc83c3f14671f528ff2",
+    "url": "/first/static/css/main.36d785ae.chunk.css"
+  },
+  {
+    "revision": "0cc83c3f14671f528ff2",
+    "url": "/first/static/js/main.33fb0b2f.chunk.js"
+  },
+  {
+    "revision": "44f7837f20780821d961",
+    "url": "/first/static/js/runtime~main.6a60995a.js"
+  },
+  {
+    "revision": "9162cf41c7fa6eafdcbb",
+    "url": "/first/static/js/2.518c7694.chunk.js"
+  },
+  {
+    "revision": "cd9dcf72c3151b386634",
+    "url": "/first/static/js/3.3331c834.chunk.js"
+  },
+  {
+    "revision": "cd1535800477429c74dc81340ab5c798",
+    "url": "/first/index.html"
+  }
+];
