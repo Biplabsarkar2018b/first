0a5daf9e9c6493ce0c475593af2f2f98c30d58f8
Biplab Sarkar <94435217+Biplabsarkar2018b@users.noreply.github.com>
2022-12-01 15:27:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from "./components/About";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState("light");
